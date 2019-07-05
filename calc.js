c107ccfe45cac83ce320110ad0cdbc66f42e85d4
Martin Lovell <gblovelm@gmail.com>
2019-07-05 14:55:14 +0100

    first working version

--- calc.js
+++ calc.js
@@ -1,18 +1,22 @@
 
-// Wait for button to be pressed by checking for a click on any button
-// Which button was Clicked - operation or number?
-// Separate function for each operation - takes in interim calc and which button pressed.
-// Assume only one button pressed at any one time.
-// If operation - set flag
-// If number - check if a flag has been set.
-// If no flag set then store the number
-// if flag set then perform calculation and return the result
-// if math operators clicked more than once between numbers then just uses the last one
+// Author:  Martin Lovell
+// Date:  05/07/2019
+// Version: 1.0    (first working version)
+// *********************************************************
+// REVISION CONTROL
+// Using Git source control
+// *********************************************************
+//  **** DESCRIPTION ****
+// Calculator function
+// Functions add, subtract, multiply, divide, shift right & clear screen
+// Can chain numerical operations
+// Rounds to nearest 0.5 for division so only integers are used
+// No exponent provided (yet) for large numbers
+
 
 const output = document.querySelector('.answer-cell');
 let screenNum = 0;
 let interimCalc = 0;
-let flag = "";
 let lastClicked = 0;
 let clicked = 0;
 let lastOperator = "";
@@ -34,12 +38,13 @@ function buttonPressed(clicked) {
     }
 }
 
-function findOperation(calcFunction) {  // last button pressed was non-integer
+function findOperation(calcFunction) {  
+    // last button pressed was non-integer
     switch (calcFunction) {
         case "C":   
             output.innerText = "0";
             interimCalc = 0;
-            lastClicked = 0;
+            lastClicked = calcFunction;
         break;
         case "→":
             if ( output.innerText !== "0" && output.innerText.length > 1) {
@@ -48,72 +53,98 @@ function findOperation(calcFunction) {  // last button pressed was non-integer
                 output.innerText = 0;
             }
         break;
-        case "÷":  // if screen value is greater than zero then output the new calc
-            if ( isNaN(parseInt(lastClicked)) ) {  // there has already been a calc
-
+        case "÷":  // if previous button was a number
+            if ( !isNaN(parseInt(lastClicked)) ) {
+                if ( interimCalc === 0 ) {
+                    interimCalc = output.innerText;
+                } else {  // do the calculation and print to screen
+                    output.innerText = stringToOp(lastOperator, interimCalc, output.innerText);
+                    interimCalc = output.innerText;
+                }
+            } 
+        break;
+        case "×":  // if previous button was a number
+            if ( !isNaN(parseInt(lastClicked)) ) {
+                if ( interimCalc === 0 ) {
+                    interimCalc = output.innerText;
+                } else {  // do the calculation and print to screen
+                    output.innerText = stringToOp(lastOperator, interimCalc, output.innerText);
+                    interimCalc = output.innerText;
+                }
             }
-                console.log("Im ÷");
         break;
-        case "×":
-                console.log("Im ×");
+        case "-":  // if previous button was a number
+            if ( !isNaN(parseInt(lastClicked)) ) {
+                if ( interimCalc === 0 ) {
+                    interimCalc = output.innerText;
+                } else {  // do the calculation and print to screen
+                    output.innerText = stringToOp(lastOperator, interimCalc, output.innerText);
+                    interimCalc = output.innerText;
+                }
+            }
         break;
-        case "-":
-                console.log("Im -");
+        case "+":  // if previous button was a number
+            if ( !isNaN(parseInt(lastClicked)) ) {
+                if ( interimCalc === 0 ) {
+                    interimCalc = output.innerText;
+                } else {  // do the calculation and print to screen
+                    output.innerText = stringToOp(lastOperator, interimCalc, output.innerText);
+                    interimCalc = output.innerText;
+                }
+            }
         break;
-        case "+":
-                console.log("Im +");
-
+        case "=":  // if previous button was a number or shift arrow
+            if ( !isNaN(parseInt(lastClicked)) || lastClicked === "→") {
+                // do the calculation and print to screen
+                output.innerText = stringToOp(lastOperator, interimCalc, output.innerText);
+                interimCalc = output.innerText;
+            } 
         break;
-        case "=":
-                output.innerText = interimCalc;
-                console.log("Im =");
-        break;
-
         default:
             console.log("You shouldn't be here!!");
     }
-     if (calcFunction !== "C" ) {
-         lastClicked = calcFunction; 
-     }
+    if (calcFunction !== "→" ){ 
+        lastOperator = calcFunction;
+    };
+    lastClicked = calcFunction;
+
+    function stringToOp(lastOp, lastCalc, screenVal) {
+        let result = 0;
+        let x = 0;
+        let y = 0;
+
+        switch(lastOp) {
+            case "+": result = plus(parseInt(lastCalc), parseInt(screenVal)); break;
+            case "-": result = minus(parseInt(lastCalc), parseInt(screenVal)); break;
+            case "×": result = multiply(parseInt(lastCalc), parseInt(screenVal)); break;
+            case "÷": result = divide(parseInt(lastCalc), parseInt(screenVal)); break;
+            default: "Uh oh, there's a problem with the last Operator.."
+        }
+        return result;
+
+        function plus( x, y ) { return ( x + y ); }; 
+        function minus( x, y ) { return ( x - y ); }; 
+        function multiply( x, y ) { return ( x * y ); }; 
+        function divide( x, y ) { return ( Math.round( x / y ) ); };
+    }
 }
 
-function inputNumberToScreen(clicked ) {  // the latest click was a number
-    console.log(clicked);
-    console.log(lastClicked);
-    if (!isNaN(parseInt(lastClicked))) {  // the previous click was a number too
+function inputNumberToScreen(clicked ) {  // the latest button was a number
 
+    if (!isNaN(parseInt(lastClicked)) || lastClicked === "C") {  
+        // the previous button was a number too
         if ( output.innerText !== "0" && output.innerText.length < 16) {
             output.innerText = output.innerText + clicked;
-            lastClicked = lastClicked + clicked;
         } else {  // dont want leading zeros so overwrite it with last click value
             if (output.innerText.length !== 16) { 
-                output.innerText = clicked;
-                lastClicked = clicked;
+                output.innerText = clicked;  // first digit to screen
             }
         }
-        // previous click was a mathematical operator      
+    // the previous button was a mathematical operator      
     } else if ( (lastClicked === "×" || "÷" || "+" || "-") && output.innerText !== "0" ) {
-        // Do the calculation and store as intermediate value, not on the screen
-        console.log('You\'re in the right place');
-        
-        switch (lastClicked) {
-            case "×" :
-                interimCalc = parseInt(output.innerText) * parseInt(clicked); console.log(interimCalc);
-            break;
-            case "÷":
-                interimCalc = parseInt(output.innerText) / parseInt(clicked); console.log(interimCalc);
-            break;
-            case "+":
-                interimCalc = parseInt(output.innerText) + parseInt(clicked); console.log(interimCalc);
-                break;
-            case "-":
-                interimCalc = parseInt(output.innerText) - parseInt(clicked); console.log(interimCalc);
-            break;
-        }
-        output.innerText = clicked; 
-        lastClicked = clicked;
-        
+        output.innerText = clicked;               
     }
+    lastClicked = clicked;
 }
 
 checkForInput();
